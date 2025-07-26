4257ff1bfb69e9b89b1f8fbdfb707e7385419575
shivammishraa25 <shivam.m4464@gmail.com>
2025-07-26 22:51:03 +0530

    nav is done, hero text and hero bg is with parallax is done, now need to add the 3d model

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Navbar from './sections/Navbar.jsx';
+import Hero from './sections/Hero.jsx';
 
 function App() {
   return (
     <div className='container mx-auto max-w-7xl'>
+      <Navbar />
+      <Hero />
+      <section className='min-h-screen'></section>
+      <section className='min-h-screen'></section>
+      <section className='min-h-screen'></section>
+      <section className='min-h-screen'></section>
+      <section className='min-h-screen'></section>
+      <section className='min-h-screen'></section>
       {/* nav
       hero
       about
